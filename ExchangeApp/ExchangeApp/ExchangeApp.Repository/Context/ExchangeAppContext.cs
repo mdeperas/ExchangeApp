43d395d92747957116506ab48c0d23ed4375b419
Marcin Deperas <mdeperas@future-processing.com>
2017-05-15 15:14:46 +0200

    Second commit

---
+++ ExchangeApp/ExchangeApp/ExchangeApp.Repository/Context/ExchangeAppContext.cs
@@ -0,0 +1,23 @@
+﻿using ExchangeApp.Repository.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ExchangeApp.Repository.Context
+{
+    public class ExchangeAppContext : IdentityDbContext<ExchangeAppContext>
+    {
+        public ExchangeAppContext() : base("ExchangeAppContext")
+        {
+        }
+
+        public ExchangeAppContext(string nameOrConnectionString) : base(nameOrConnectionString)
+        {
+        }
+
+        public DbSet<ExchangeAppUser> ExchangeAppUsers { get; set; }
+    }
+}
